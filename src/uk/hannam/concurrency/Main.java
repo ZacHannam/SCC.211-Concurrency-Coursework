6c01abcf5abe2458ce4765391af4b621399d2e2a
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 03:22:57 +0100

    Added a new usage message in main

--- src/uk/hannam/concurrency/Main.java
+++ src/uk/hannam/concurrency/Main.java
@@ -1,9 +1,20 @@
 package uk.hannam.concurrency;
 
+import uk.hannam.concurrency.workers.Workers;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+
+    public static String getUsageMessage() {
+        StringBuilder usage = new StringBuilder("Usage: ");
+        for(Workers workerType : Workers.values()) {
+            usage.append("<n. ").append(workerType.toString()).append("> ");
+        }
+        usage.append("<flag [0-1]>");
+        return usage.toString();
+    }
 
     public static void main(String[] args) {
         try {
@@ -19,7 +30,7 @@ public class Main {
             warehouse.run();
 
         } catch(NumberFormatException | IndexOutOfBoundsException e) {
-            throw new RuntimeException("Usage: <n. Add Workers> <n. Remove Workers> <flag [0-1]>");
+            throw new RuntimeException(getUsageMessage());
         }
     }
 }
